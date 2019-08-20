3a137e587188563225221c919a4b310ed62ee816
Tony Baker <this.tonybaker@gmail.com>
2019-08-20 20:13:33 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    User.create(user_params)
   end
 
   def user_params
-
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
