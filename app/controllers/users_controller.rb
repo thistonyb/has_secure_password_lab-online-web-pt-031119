15d016695b32b58a0063fe500abc27e06909a5c3
Tony Baker <this.tonybaker@gmail.com>
2019-08-20 20:47:38 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,10 @@ class UsersController < ApplicationController
   end
 
   def create
+    return redirect_to '/users/new' if params[:user][:password] != params[:user][:password_confirmation]
     User.create(user_params)
+    session[:user_id] = User.last.id
+    redirect_to '/'
   end
 
   def user_params
